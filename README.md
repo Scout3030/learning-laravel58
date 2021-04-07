bccbb4244441e73ae819250ca61a7f316706da35
Roberth <rrodriguezj@uni.pe>
2021-04-06 21:43:11 -0500

    feature | docker finished

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# README
+# ARCHIVO CONFIGURACIÓN
+
+- Correr script, no crear variables nuevamente. Si las migraciones no se crean, correr dos veces el script.
+- Las variables del .env se cargan en el docker-file.
 
 ## Levantar proyecto con docker
-- (Las variables del .env se cargan en el docker-file)
+
 ### Conceder permisos 
 ````
 chmod +x script.sh
@@ -11,3 +14,25 @@ chmod +x script.sh
 ````
 ./script.sh
 ````
+## Comandos importantes
+
+### Listar contenedores
+````
+docker-compose ps -f name=learnig*
+````
+
+### Ver logs en nginx
+````
+docker-compose logs nginx
+````
+
+### Eliminar contenedores
+````
+docker ps -f name=learnig* -q | xargs docker rm -f
+````
+
+<br>  
+Más información en: 
+
+[Digital Ocean Tutorial](https://www.digitalocean.com/community/tutorials/how-to-install-and-set-up-laravel-with-docker-compose-on-ubuntu-20-04)
+
--- script.sh
+++ script.sh
@@ -3,37 +3,61 @@
 echo "Configurar el .env"
 cp .env.example .env
 
-MYSQL_DATABASE=${2:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5 ; echo '')}
-MYSQL_USERNAME=${2:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5 ; echo '')}
-MYSQL_PASSWORD=${2:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20 ; echo '')}
+read -p "¿Establecer nuevas variables mysql en .env?' [Y/n]: " optiona
+if [[ $optiona =~ ^[Yy]$ ]]
+then
+    MYSQL_DATABASE=${1:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5 ; echo '')}
+    MYSQL_USERNAME=${2:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5 ; echo '')}
+    MYSQL_PASSWORD=${3:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20 ; echo '')}
 
-echo "Establecer database mysql"
-sed -i "/DB_DATABASE=/c\DB_DATABASE=LEARNING$MYSQL_DATABASE" .env
+    echo "Establecer database mysql"
+    sed -i "/DB_DATABASE=/c\DB_DATABASE=learning${MYSQL_DATABASE,,}" .env
 
-echo "Establecer usuario mysql"
-sed -i "/DB_USERNAME=/c\DB_USERNAME=USR$MYSQL_USERNAME" .env
+    echo "Establecer usuario mysql"
+    sed -i "/DB_USERNAME=/c\DB_USERNAME=usr${MYSQL_USERNAME,,}" .env
 
-echo "Establecer contraseña mysql"
-sed -i "/DB_PASSWORD=/c\DB_PASSWORD=$MYSQL_PASSWORD" .env
+    echo "Establecer contraseña mysql"
+    sed -i "/DB_PASSWORD=/c\DB_PASSWORD=${MYSQL_PASSWORD,,}" .env
+fi
 
 echo "Establecer conexion a db"
 sed -i "/DB_HOST=/c\DB_HOST=db" .env
 
-echo "Construir los contenedores"
-docker-compose build app
+read -p "¿Construir conenedores?' [Y/n]: " optionb
+if [[ $optionb =~ ^[Yy]$ ]]
+then
+    echo "Construir los contenedores"
+    docker-compose build app
+fi
 
-echo "Poner en funcionamiento"
-docker-compose up -d
+read -p "¿Poner en funcionamiento conenedores?' [Y/n]: " optionc
+if [[ $optionc =~ ^[Yy]$ ]]
+then
+    echo "Poner en funcionamiento"
+    docker-compose up -d
+fi
 
 echo "Instalar composer"
 docker-compose exec app composer install
 
+echo "Dar permisos al storage"
+docker-compose exec app chmod -R 777 storage
+
 echo "Generar llaves"
 docker-compose exec app php artisan key:generate
 
-# echo "Linkear storage"
-# docker-compose exec app php artisan storage:link
+echo "Linkear storage"
+docker-compose exec app php artisan storage:link
 
-echo "Correr migraciones"
+echo "Crear migraciones"
 docker-compose exec app php artisan migrate
+
+read -p "¿Crear seeds?' [Y/n]: " optiond
+if [[ $optiond =~ ^[Yy]$ ]]
+then
+    docker-compose exec app php artisan db:seed
+fi
+
+docker-compose ps
+
 
